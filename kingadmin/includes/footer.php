75a77437ec75a35a753bdafcb8daccb44cd006b3
Kadam-Tushar <tushar.kadam@somaiya.edu>
2019-03-31 01:41:56 +0530

    Template added

---
+++ kingadmin/includes/footer.php
@@ -0,0 +1,58 @@
+<!-- FOOTER -->
+<footer class="footer">
+		&copy; 2014-2015 The Develovers
+	</footer>
+	<!-- END FOOTER -->
+
+	<!-- STYLE SWITCHER -->
+	<div class="del-style-switcher">
+		<div class="del-switcher-toggle toggle-hide"></div>
+		<form>
+			<section class="del-section del-section-skin">
+				<h5 class="del-switcher-header">Choose Skins:</h5>
+				<ul>
+					<li><a href="#" title="Slate Gray" class="switch-skin slategray" data-skin="assets/css/skins/slategray.css">Slate Gray</a></li>
+					<li><a href="#" title="Dark Blue" class="switch-skin darkblue" data-skin="assets/css/skins/darkblue.css">Dark Blue</a></li>
+					<li><a href="#" title="Dark Brown" class="switch-skin darkbrown" data-skin="assets/css/skins/darkbrown.css">Dark Brown</a></li>
+					<li><a href="#" title="Light Green" class="switch-skin lightgreen" data-skin="assets/css/skins/lightgreen.css">Light Green</a></li>
+					<li><a href="#" title="Orange" class="switch-skin orange" data-skin="assets/css/skins/orange.css">Orange</a></li>
+					<li><a href="#" title="Red" class="switch-skin red" data-skin="assets/css/skins/red.css">Red</a></li>
+					<li><a href="#" title="Teal" class="switch-skin teal" data-skin="assets/css/skins/teal.css">Teal</a></li>
+					<li><a href="#" title="Yellow" class="switch-skin yellow" data-skin="assets/css/skins/yellow.css">Yellow</a></li>
+				</ul>
+				<button type="button" class="switch-skin-full fulldark" data-skin="assets/css/skins/fulldark.css">Full Dark</button>
+				<button type="button" class="switch-skin-full fullbright" data-skin="assets/css/skins/fullbright.css">Full Bright</button>
+			</section>
+			<label class="fancy-checkbox">
+				<input type="checkbox" id="fixed-top-nav"><span>Fixed Top Navigation</span>
+			</label>
+			<p><a href="#" title="Reset Style" class="del-reset-style">Reset Style</a></p>
+		</form>
+	</div>
+	<!-- END STYLE SWITCHER -->
+
+	<!-- Javascript -->
+	<script src="assets/js/jquery/jquery-2.1.0.min.js"></script>
+	<script src="assets/js/bootstrap/bootstrap.min.js"></script>
+	<script src="assets/js/plugins/modernizr/modernizr.js"></script>
+	<script src="assets/js/plugins/bootstrap-tour/bootstrap-tour.custom.js"></script>
+	<script src="assets/js/king-common.js"></script>
+	<script src="demo-style-switcher/assets/js/deliswitch.js"></script>
+	<script src="assets/js/plugins/stat/jquery.easypiechart.min.js"></script>
+	<script src="assets/js/plugins/raphael/raphael-2.1.0.min.js"></script>
+	<script src="assets/js/plugins/stat/flot/jquery.flot.min.js"></script>
+	<script src="assets/js/plugins/stat/flot/jquery.flot.resize.min.js"></script>
+	<script src="assets/js/plugins/stat/flot/jquery.flot.time.min.js"></script>
+	<script src="assets/js/plugins/stat/flot/jquery.flot.pie.min.js"></script>
+	<script src="assets/js/plugins/stat/flot/jquery.flot.tooltip.min.js"></script>
+	<script src="assets/js/plugins/jquery-sparkline/jquery.sparkline.min.js"></script>
+	<script src="assets/js/plugins/datatable/jquery.dataTables.min.js"></script>
+	<script src="assets/js/plugins/datatable/dataTables.bootstrap.js"></script>
+	<script src="assets/js/plugins/jquery-mapael/jquery.mapael.js"></script>
+	<script src="assets/js/plugins/raphael/maps/usa_states.js"></script>
+	<script src="assets/js/king-chart-stat.js"></script>
+	<script src="assets/js/king-table.js"></script>
+	<script src="assets/js/king-components.js"></script>
+</body>
+
+</html>
